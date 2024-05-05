e755bfe65b2dd785a3e8cb72d98c49c8876e63a5
hahoang12 <hachichuatoi2003@gmail.com>
2024-05-05 16:53:41 +0700

    shoppingCart

---
+++ PhoneSaleAPI/DTO/ShoppingCart/CartDetailDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace PhoneSaleAPI.DTO.ShoppingCart
+{
+    public class CartDetailDTO
+    {
+        public string ShoppingCartId { get; set; } 
+        public string ProductId { get; set; } 
+        public string ColorName { get; set; } 
+        public int StorageGb { get; set; }
+        public int? Amount { get; set; }
+        public int? Total { get; set; }
+    }
+}
