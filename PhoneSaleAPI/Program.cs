ea48dbd0417a38f79a9f825c96b74726a6725e39
KhanhCauCa <phamngockhanh2723@gmail.com>
2024-05-08 17:19:01 +0700

    update api

--- PhoneSaleAPI/Program.cs
+++ PhoneSaleAPI/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddCors(option => option.AddDefaultPolicy(policy =>
         policy.WithOrigins("http://127.0.0.1:5500").AllowAnyHeader().AllowAnyMethod().AllowCredentials()));
 
 builder.Services.AddDbContext<PhoneManagementContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("dbPhoneManagement")));
 
 
 var app = builder.Build();
